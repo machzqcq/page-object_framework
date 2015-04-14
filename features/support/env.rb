e848291b21f10eb8e82e2d37b5b5f6847affbb5e
Pradeep <macharla.pradeep.kumar@gmail.com>
2015-04-14 16:00:14 -0400

    Rcov coverage support

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,6 +7,17 @@ require 'nokogiri'
 require 'active_support/core_ext'
 require 'saucelabs'
 
+require 'active_support/core_ext'
+require 'simplecov'
+require 'simplecov-rcov'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::RcovFormatter
+]
+
+SimpleCov.start
+
 
 require './lib/page_helper'
 require './lib/data_helper'

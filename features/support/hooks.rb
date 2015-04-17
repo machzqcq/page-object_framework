d1e8df1b70f6aab8e8ad41556ec9553fafc43351
Pradeep <macharla.pradeep.kumar@gmail.com>
2015-04-17 15:55:48 -0400

    archiving results

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -23,5 +23,23 @@ After do |scenario|
 
 end
 
+at_exit do
+  ENV['ARCHIVE_RESULTS'] = 'no' if ENV['ARCHIVE_RESULTS'].nil?
+  if ENV['ARCHIVE_RESULTS']=="yes"
+    Dir.mkdir("resultsarchive") if Dir["resultsarchive"].empty?
+    folder = Dir.pwd
+    input_filenames = ['results.html']
+
+    zipfile_name = "#{Date.today}_results.zip"
+
+    Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
+      input_filenames.each do |filename|
+        zipfile.add(filename, folder + '/' + filename)
+      end
+    end
+    FileUtils.mv(zipfile_name,"resultsarchive/#{zipfile_name}")
+  end
+end
+
 
 
